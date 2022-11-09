31de22f3ea2c964d5d353593a0e2aff37bc9077e
Felix Schulze <f.schulze.2003@web.de>
2022-11-09 12:23:38 +0100

    Added more activation functions.

--- EasyNNFramework/NEAT/Neuron.cs
+++ EasyNNFramework/NEAT/Neuron.cs
@@ -17,12 +17,22 @@ namespace EasyNNFramework {
             function = _function;
         }
 
+        private const float l = 1.0507009873554804934193349852946f;
+        private const float a = 1.6732632423543772848170429916717f;
         public static float getFunctionValue(ActivationFunction _function, float sum) {
             switch (_function) {
                 case ActivationFunction.GELU:
                     return 0.5f * sum * (1 + (float)Math.Tanh(Math.Sqrt(2f / Math.PI) * (sum + 0.044715f * Math.Pow(sum, 3))));
                 case ActivationFunction.TANH:
                     return (float)Math.Tanh(sum);
+                case ActivationFunction.SIGMOID:
+                    return 1.0f / (1.0f + (float)Math.Exp(-sum));
+                case ActivationFunction.SWISH:
+                    return sum / (1.0f + (float)Math.Exp(-sum));
+                case ActivationFunction.RELU:
+                    return Math.Max(0, sum);
+                case ActivationFunction.SELU:
+                    return sum > 0 ? l * sum : l * a * ((float) Math.Exp(sum) - 1f);
                 default:
                     return sum;
             }
@@ -54,6 +64,6 @@ namespace EasyNNFramework {
 
     public enum NeuronType {Input = 0, Hidden = 1, Action = 2}
 
-    public enum ActivationFunction {GELU = 0, TANH = 1}
+    public enum ActivationFunction {GELU = 0, TANH = 1, SIGMOID = 2, SWISH = 3, RELU = 4, SELU = 5}
 
 }
