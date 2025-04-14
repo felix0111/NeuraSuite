430c9ed0b6856dc39845fe8851ea2aa2705cb688
Felix <f.schulze.2003@web.de>
2025-04-14 19:38:44 +0200

    Added documentation and explanations. AverageFitness() can now also calculate the median instead of the simple mathematically average. ImprovedSince() and StepsSinceImprovement have been removed to avoid confusion and useless bloat. Renamed MaxSpecies to MaxSpeciesPerPopulation to avoid confusion.
    Commit 5/6!

--- EasyNNFramework/NEAT/Species.cs
+++ EasyNNFramework/NEAT/Species.cs
@@ -11,6 +11,9 @@ namespace EasyNNFramework.NEAT {
     [Serializable]
     public class Species {
 
+        /// <summary>
+        /// The network this species is based upon. Every network in this species should be compatible to the representative.
+        /// </summary>
         public Network Representative;
 
         public readonly int SpeciesID;
@@ -18,7 +21,6 @@ namespace EasyNNFramework.NEAT {
         public Dictionary<int, Network> AllNetworks;
 
         public float BestAverageFitness = -1f;
-        public int StepsSinceImprovement;
 
         public Species(int speciesId, Network representative) {
             Representative = new Network(-1, representative);
@@ -28,6 +30,10 @@ namespace EasyNNFramework.NEAT {
             AllNetworks = new Dictionary<int, Network>();
         }
 
+        /// <summary>
+        /// Checks if a network is compatible to this species.
+        /// </summary>
+        /// <param name="addToSpecies">If true, the network will be automatically added to this species if compatible.</param>
         public bool CheckCompatibility(Network network, SpeciationOptions options, bool addToSpecies) {
             bool comp = NEATUtility.Distance(Representative, network, options) <= options.CompatabilityThreshold;
 
@@ -35,6 +41,9 @@ namespace EasyNNFramework.NEAT {
             return comp;
         }
 
+        /// <summary>
+        /// Adds a network to this species. You should normally <see cref="CheckCompatibility"/> beforehand!
+        /// </summary>
         public bool AddToSpecies(Network network) {
             network.SpeciesID = SpeciesID;
 
@@ -43,51 +52,61 @@ namespace EasyNNFramework.NEAT {
             return true;
         }
 
+        /// <summary>
+        /// Removes a network from this species.
+        /// </summary>
         public bool RemoveFromSpecies(Network network) {
             network.SpeciesID = -1;
             return AllNetworks.Remove(network.NetworkID);
         }
 
-        //basically the average of all networks' (adjusted) fitness
-        public float AverageFitness(bool useAdjFitness) {
+        /// <summary>
+        /// Calculates the average fitness of all networks in this species.
+        /// </summary>
+        /// <param name="useAdjFitness">If true, penalizes larger species and gives smaller ones an advantage.</param>
+        /// <param name="median">If true, might filter out statistical outliers much better.</param>
+        // TODO median needs more testing
+        public float AverageFitness(bool useAdjFitness, bool median) {
+            if (AllNetworks.Count == 0) return 0f;
 
             float val = 0;
-            foreach (var network in AllNetworks) {
-                val += useAdjFitness ? network.Value.Fitness / AllNetworks.Count : network.Value.Fitness;
+            if (median) {
+                //calculate median, order by fitness and select the network in the middle
+                val = AllNetworks.OrderByDescending(o => o.Value.Fitness).ElementAt( (int)Math.Ceiling(AllNetworks.Count / 2f) - 1).Value.Fitness;
+            } else {
+                //calculate average, sum all (adjusted) fitness and divide by number of networks
+                foreach (var network in AllNetworks) {
+                    val += useAdjFitness ? network.Value.Fitness / AllNetworks.Count : network.Value.Fitness;
+                }
+                val /= AllNetworks.Count;
             }
-            val = val / AllNetworks.Count;
 
-            if (float.IsNaN(val)) val = 0;
+            if(val > BestAverageFitness) BestAverageFitness = val;  //update best average fitness of this species
 
             return val;
         }
 
-        //checks if species has improved at least once since x generations
-        public bool ImprovedSince(bool useAdjFitness, int gens) {
-            float fitn = AverageFitness(useAdjFitness);
-            if (BestAverageFitness >= fitn) { //no improvement
-                StepsSinceImprovement++;
-            } else {
-                StepsSinceImprovement = 0;  //improvement
-                BestAverageFitness = fitn;
-            }
+        /// <summary>
+        /// Used to create a population with only the best x networks of this species where the amount of different networks is defined by <see cref="networkVariety"/>.
+        /// <br/>
+        /// When every fitness is 0, returns empty list otherwise returns a list of tuples: (network ID, amount)
+        /// </summary>
+        /// <param name="targetPopulationSize">The total amount of networks in the new population.</param>
+        /// <param name="networkVariety">
+        /// How many of the first best networks are used to create the new population.
+        /// Basically determines the variety of networks in the new population.
+        /// </param>
+        public List<(int, int)> CreateSpeciesPopulation(int targetPopulationSize, int networkVariety) {
 
-            return StepsSinceImprovement <= gens;
-        }
-
-        
-
-        //returns empty when every fitness is 0
-        //values lie between 0 and 1 and add up to 1
-        //variety of networks returned is determined by eliteCount, maxmimum is target population size
-        public List<(int, int)> PopulationSize(int targetPopulationSize, int eliteCount) {
-
+            //take the best x networks with x = eliteCount (maximum is targetSpeciesSize)
             List<(int, int)> newArr = new List<(int, int)>();
-            var orderedAndReduced = AllNetworks.OrderByDescending(o => o.Value.Fitness).Take(Math.Min(targetPopulationSize, eliteCount)).ToArray();
+            var orderedAndReduced = AllNetworks.OrderByDescending(o => o.Value.Fitness).Take(Math.Min(targetPopulationSize, networkVariety)).ToArray();
 
+            //sum fitness of all selected networks
             float fitnessSum = orderedAndReduced.Sum(o => o.Value.Fitness);
             if (fitnessSum == 0) return newArr;
 
+            //linearly scales the fitness of each selected network to the targetSpeciesSize
             foreach (var network in orderedAndReduced) {
                 newArr.Add((network.Key, (int) Math.Round((network.Value.Fitness / fitnessSum) * targetPopulationSize)));
             }
@@ -96,20 +115,60 @@ namespace EasyNNFramework.NEAT {
         }
     }
 
+    /// <summary>
+    /// Options for defining the behaviour when assigning networks to species.
+    /// </summary>
     public struct SpeciationOptions {
-        public float DisjointFactor, WeightFactor, CompatabilityThreshold;
 
-        public int MaxSpecies;
+        /// <summary>
+        /// If the distance of two networks are smaller or equal than this threshold, then they belong to one species.
+        /// <br/> <br/>
+        /// A value of 0.3 is suggested. A higher threshold may decrease the amount of species.
+        /// </summary>
+        public float CompatabilityThreshold;
 
+        /// <summary>
+        /// Determines how much influence the amount of disjoints over connection amount has on species compatability.
+        /// <br/> <br/>
+        /// A value of 1 is suggested. A higher value may increase the amount of individual species.
+        /// </summary>
+        public float DisjointFactor;
+
+        /// <summary>
+        /// Determines how much influence the absolute difference in weights has on species compatability.
+        /// <br/> <br/>
+        /// A value of 0 is suggested. A value higher than 0.1 may increase the amount of individual species drastically.
+        /// Counteract by increasing <see cref="CompatabilityThreshold"/>!
+        /// </summary>
+        public float WeightFactor;
+
+        /// <summary>
+        /// Determines how many species are allowed.
+        /// <br/> <br/>
+        /// A value between <code>1</code> and <code>total amount of networks</code> is suggested.
+        /// A higher value may give bad performing species an advantage.
+        /// </summary>
+        // TODO this does not really have anything to do with speciation, only used by CreatePopulation, move to population class in future
+        public int MaxSpeciesPerPopulation;
+
+        /// <summary>
+        /// When true, penalizes larger species and gives smaller ones an advantage.
+        /// <br/> <br/>
+        /// Set to true if you want to give species with low average fitness an advantage when creating new populations with <see cref="Neat.CreatePopulation"/>.
+        /// </summary>
+        // TODO move variable to another place?
         public bool UseAdjustedFitness;
 
-        //compatability threshold - higher number means more differences allowed
-        //maxSpecies - the more species are allowed, the more lower performing specie can evolve.
-        public SpeciationOptions(float disjointFactor, float weightFactor, float compatabilityThreshold, int maxSpecies, bool useAdjustedFitness) {
+        /// <param name="disjointFactor"><inheritdoc cref="DisjointFactor"/></param>
+        /// <param name="weightFactor"><inheritdoc cref="WeightFactor"/></param>
+        /// <param name="compatabilityThreshold"><inheritdoc cref="CompatabilityThreshold"/></param>
+        /// <param name="maxSpeciesPerPopulation"><inheritdoc cref="MaxSpeciesPerPopulation"/></param>
+        /// <param name="useAdjustedFitness"><inheritdoc cref="UseAdjustedFitness"/></param>
+        public SpeciationOptions(float disjointFactor = 1f, float weightFactor = 0f, float compatabilityThreshold = 0.3f, int maxSpeciesPerPopulation = 10, bool useAdjustedFitness = false) {
             DisjointFactor = disjointFactor;
             WeightFactor = weightFactor;
             CompatabilityThreshold = compatabilityThreshold;
-            MaxSpecies = maxSpecies;
+            MaxSpeciesPerPopulation = maxSpeciesPerPopulation;
             UseAdjustedFitness = useAdjustedFitness;
         }
     }
