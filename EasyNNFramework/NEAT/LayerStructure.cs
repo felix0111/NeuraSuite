c549c4d58ec577383bc6a8f30e2669ab251c1b2a
Felix <f.schulze.2003@web.de>
2025-04-15 21:36:03 +0200

    Changed name from layerArray to LayerArray and neuronLayerDict to _neuronLayerDict.

--- EasyNNFramework/NEAT/LayerStructure.cs
+++ EasyNNFramework/NEAT/LayerStructure.cs
@@ -8,14 +8,14 @@ namespace EasyNNFramework.NEAT {
 
     [Serializable]
     public struct LayerStructure {
-        public List<int>[] layerArray;
-        private Dictionary<int, int> neuronLayerDict;
+        public List<int>[] LayerArray;
+        private Dictionary<int, int> _neuronLayerDict;
 
         private int _highestLayer;
 
         public LayerStructure(in Network network) {
-            layerArray = Array.Empty<List<int>>();
-            neuronLayerDict = new Dictionary<int, int>(network.Neurons.Count); //key = neuron id, value = layer
+            LayerArray = Array.Empty<List<int>>();
+            _neuronLayerDict = new Dictionary<int, int>(network.Neurons.Count); //key = neuron id, value = layer
             _highestLayer = -1;
 
             // TODO directly add neurons to layerArray without using neuronLayerDict as a buffer, maybe adding layer variable in neuron class?
@@ -23,7 +23,7 @@ namespace EasyNNFramework.NEAT {
             //add input neurons
             _highestLayer = 1;
             foreach (var input in network.InputNeurons) {
-                neuronLayerDict.Add(input.ID, 1);
+                _neuronLayerDict.Add(input.ID, 1);
             }
 
             //calculate hidden neuron layers by back propagating connections
@@ -34,34 +34,35 @@ namespace EasyNNFramework.NEAT {
             //action neurons must be manually added because they are not seen in the back propagation process
             _highestLayer++;
             foreach (var neuron in network.ActionNeurons) {
-                neuronLayerDict.Add(neuron.ID, _highestLayer);
+                _neuronLayerDict.Add(neuron.ID, _highestLayer);
             }
 
             //init layer array
             //make sure to call this after GetLayer because that's where _highestLayer is defined
-            layerArray = new List<int>[_highestLayer];
-            for (int i = 0; i < _highestLayer; i++) layerArray[i] = new List<int>();
+            LayerArray = new List<int>[_highestLayer];
+            for (int i = 0; i < _highestLayer; i++) LayerArray[i] = new List<int>();
 
             //populate layer array
             //input neurons
             for (int i = 0; i < network.InputNeurons.Length; i++) {
-                layerArray[0].Add(network.InputNeurons[i].ID);
+                LayerArray[0].Add(network.InputNeurons[i].ID);
             }
             //hidden neurons
             for (int i = 0; i < network.HiddenNeurons.Length; i++) {
-                int layerIndex = neuronLayerDict[network.HiddenNeurons[i].ID] - 1;
-                layerArray[layerIndex].Add(network.HiddenNeurons[i].ID);
+                int layerIndex = _neuronLayerDict[network.HiddenNeurons[i].ID] - 1;
+                LayerArray[layerIndex].Add(network.HiddenNeurons[i].ID);
             }
             //action neurons
             for (int i = 0; i < network.ActionNeurons.Length; i++) {
-                layerArray[_highestLayer - 1].Add(network.ActionNeurons[i].ID);
+                LayerArray[_highestLayer - 1].Add(network.ActionNeurons[i].ID);
             }
         }
 
         //automatically adds to internal neuron layer dict
         // TODO rewrite function to also handle input neurons which might allow for more performant ways of creating the layer structure
         public int GetLayer(int neuronID, in Network network) {
-            if (neuronLayerDict.TryGetValue(neuronID, out int v)) return v;
+            if (_neuronLayerDict.TryGetValue(neuronID, out int v)) return v;
+
 
             int highestSourceNeuronLayer = 1;
             for (int i = 0; i < network.Neurons[neuronID].IncommingConnections.Count; i++) {
@@ -70,7 +71,7 @@ namespace EasyNNFramework.NEAT {
                 if (l > highestSourceNeuronLayer) highestSourceNeuronLayer = l;
             }
 
-            if(!neuronLayerDict.ContainsKey(neuronID)) neuronLayerDict.Add(neuronID, highestSourceNeuronLayer + 1);
+            if(!_neuronLayerDict.ContainsKey(neuronID)) _neuronLayerDict.Add(neuronID, highestSourceNeuronLayer + 1);
 
             if (highestSourceNeuronLayer + 1 > _highestLayer) _highestLayer = highestSourceNeuronLayer + 1;
             return highestSourceNeuronLayer + 1;
