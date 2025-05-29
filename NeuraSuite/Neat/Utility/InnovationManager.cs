b9fc496631fb31ee6e2b8ecb282412197ba80c66
Felix <f.schulze.2003@web.de>
2025-05-29 11:57:33 +0200

    Added InnovationManager for keeping track of innovations and neuron ids.

---
+++ NeuraSuite/Neat/Utility/InnovationManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using NeuraSuite.Neat.Core;
+
+namespace NeuraSuite.Neat.Utility {
+
+    public class InnovationManager {
+
+        //global node counter
+        //node ids are zero based
+        private int _nodeCounter;
+
+        /// <summary>
+        /// Returns a new node id and increments the global counter.
+        /// </summary>
+        public int NewNodeId => _nodeCounter++;
+
+        //key: (startNodeId, endNodeId) value: innovation
+        //innovations are zero-based
+        private Dictionary<(int, int), int> _connectionInnovations = new();
+
+        /// <summary>
+        /// Returns the innovation for the specified connection. Returns a new innovation when not found.
+        /// </summary>
+        public int GetInnovation(int startNode, int endNode) {
+            if (_connectionInnovations.TryGetValue((startNode, endNode), out int innov)) return innov;
+
+            int newInnov = _connectionInnovations.Count;
+            _connectionInnovations.Add((startNode, endNode), newInnov);
+            return newInnov;
+        }
+    }
+}
