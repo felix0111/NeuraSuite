ab1c3690c081f1c335c1b78f44cd0631eab82e9f
Felix <f.schulze.2003@web.de>
2025-05-30 21:28:47 +0200

    Added some options.

--- NeuraSuite/Neat/NeatOptions.cs
+++ NeuraSuite/Neat/NeatOptions.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace NeuraSuite.Neat {
 
-namespace NeuraSuite.Neat {
+    public struct NeatOptions {
 
-    public class NeatOptions {
+        /// <summary>
+        /// Defines the targeted number of genomes after repopulation.
+        /// </summary>
+        public int TargetPopulationSize;
 
+        /// <summary>
+        /// Defines the amount of low-performing genomes that are removed from each species when repopulating.
+        /// </summary>
+        public double RemoveWorstPercentage;
+
+        /// <summary>
+        /// The probability for crossover to happen when creating new offspring. Otherwise offspring is a direct clone.
+        /// </summary>
+        public double CrossoverChance;
+
+        /// <summary>
+        /// The probability for a gene to be enabled when at least one parents gene is disabled.
+        /// </summary>
+        public double EnableChance;
+
+        public NeatOptions(int targetPopulationSize, double removeWorstPercentage, double crossoverChance, double enableChance) {
+            TargetPopulationSize = targetPopulationSize;
+            RemoveWorstPercentage = removeWorstPercentage;
+            CrossoverChance = crossoverChance;
+            EnableChance = enableChance;
+        }
     }
 }
